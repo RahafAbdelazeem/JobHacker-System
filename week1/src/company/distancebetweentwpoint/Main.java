d66348fbc51eb101ce15c8f3bbbc489bcc088042
RahafAbdelazeem <85758565+RahafAbdelazeem@users.noreply.github.com>
2023-06-16 23:15:51 +0300

    Delete week1 directory

--- week1/src/company/distancebetweentwpoint/Main.java
+++
@@ -1,9 +0,0 @@
-package company.distancebetweentwpoint;
-import java.io.IOException;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-	// write your code here
-    }
-}
